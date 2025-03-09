fca53f0b8c47c33d4448ad5adcd3452500d5fefa
Yoshiko Hatanaka <yohatana@c6r3s3.42tokyo.jp>
2025-03-09 15:31:47 +0900

    single quarte surround token

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,80 +6,11 @@
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 18:22:31 by yohatana          #+#    #+#             */
-/*   Updated: 2025/03/08 17:54:10 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/03/09 15:30:02 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../includes/minishell.h"
-/*
-このコマンドを入力したとする
-$ ls | wc -l | echo ${HOGE} | sed 's/aaa/bbb/'
-
-
-次のような構造体にコマンドを分割して入れるイメージ。
-typedef struct s_cmd
-{
-  char *cmd;
-  char **args;
-  char *cmd_abs_path;
-} t_cmd
-
-↓cmdをlistで持つ
-typedef struct cmds
-{
-  t_cmd cmd;
-  t_cmd *next;
-} t_cmds
-
-
-Step1. まず "|" と " " でsplitする
-t_cmd
-{
-  char *cmd = "ls";
-  char **args = NULL;
-  char *cmd_abs_path = <lsの絶対パス>;
-}
-
-t_cmd
-{
-  char *cmd = "wc";
-  char **args = ["-l"];
-  char *cmd_abs_path = <wcの絶対パス>;
-}
-
-t_cmd
-{
-  char *cmd = "echo";
-  char **args = ["${HOGE}"]; <- まだ展開せずに文字列で持つ
-  char *cmd_abs_path = <echoの絶対パス>;
-}
-
-t_cmd
-{
-  char *cmd = "sed";
-  char **args = "['s/aaa/bbb/'"];
-  char *cmd_abs_path = <sedの絶対パス>;
-}
-
-
-Step2. single quote, double quoteを消す
-t_cmd
-{
-  char *cmd = "sed";
-  char **args = ["s/aaa/bbb/"]; <- single quote消した
-  char *cmd_abs_path = <sedの絶対パス>;
-}
-
-
-Step3. 環境変数の展開
-t_cmd
-{
-  char *cmd = "echo";
-  char **args = ["expanded_var"]; <- 展開した
-  char *cmd_abs_path = <echoの絶対パス>;
-}
-
-*/
+#include "../includes/minishell.h"
 
 typedef struct s_cmd
 {
@@ -89,92 +20,159 @@ typedef struct s_cmd
 	int		status;
 } t_cmd;
 
-typedef struct s_fds
-{
-	int	in;
-	int	out;
-}	t_fds;
-
 typedef struct s_cmd_list
 {
 	t_cmd	cmd;
 	t_cmd	*next;
 }	t_cmd_list;
 
-typedef struct s_tokens
-{
-	char		*token;
-	t_tokens	*next;
-}	t_tokens;
+typedef struct s_token t_token;
 
-static char	*single_token(int start, int end, char *line);
+typedef struct s_token
+{
+	char	*word;
+	int		expansion_flg;
+	t_token	*next;
+}	t_token;
+
+t_token		*create_token_node(char *word, int expansion_flg);
+void		add_token(t_token **head, t_token *new);
+static char	*quart_token(int start, int end, char *line);
+void		syntax_error(void);
 
 void	parser(int argc, char **envp, char *line)
 {
 	(void)argc;
 	(void)envp;
+	(void)line;
 
-	// char	**splited;
-	// char	*no_single;
 	int		i;
-	char	*token;			// 1かたまり
-	int		token_count;
-
-	// char	**token_array; // 最終的に入れる
-
 	int		j;
+	char	*word;
+	t_token	*token_head;
+
+	token_head = NULL;
 	i = 0;
 	j = 0;
+	int	expansion_flg = 0;
 	while (line[i])
 	{
+		// 'が奇数のときはエラーにする
 		if (line[i] == '\'')
 		{
 			j = i + 1;
 			while (line[j] != '\'')
 				j++;
-			if (line[j] == '\'')
+			if (line[j] == '\'' && j < (int)ft_strlen(line))
 			{
-				token = single_token(i + 1, j , line);
+				word = quart_token(i + 1, j , line);
+				add_token(&token_head, create_token_node(word, expansion_flg));
 				i = j;
-				token_count++;
+			}
+			// if (j == (int)ft_strlen(line))
+			// {
+			// 	// syntax_error()
+			// }
+		}
+		if (line[i] == '\"')
+		{
+			j = i + 1;
+			while (line[j] != '\"')
+				j++;
+			if (line[j] == '\"')
+			{
+				word = quart_token(i + 1, j , line);
+				expansion_flg = 1;syntax errorode(word, expansion_flg));
+				i = j;
+			}
+		}
+		if (line[i] == '|')
+		{
+			// create token
+			// word = "|"
+		}
+		if (line[i] == '$')
+		{
+			// ' ' '\'' "\""が来るまでtokenとして扱う
+		}
+
+		if (line[i] == ' ')
+		{
+			if (line[i - 1] != ' ' || line[i - 1] == '\'' || line[i - 1] == '\"')
+			{
+				// create token
 			}
 		}
 		i++;
 	}
 
-	// ""　ダブル($展開をする)
+	printf("=== token list check ===\n");
+	t_token *test;
+	test = token_head;
+	while (test)
+	{
+		printf("word :%s expansion flg :%d\n", test->word, test->expansion_flg);
+		test = test->next;
+	}
 
-	// |　パイプ
+	// '|'が来るまでは連結する
+	// '|'の前後にtokenがない場合(> ,'', "", $, stringはtokenになる)
 
-	// $ 展開
 }
 
-static char	*single_token(int start, int end, char *line)
+static char	*quart_token(int start, int end, char *line)
 {
-	char	*token;
+	char	*word;
 	int		i;
 
 	printf("s :%d e :%d\n", start, end);
-	token = (char *)ft_calloc(sizeof(char), end - start + 1);
-	if (!token)
+	word = (char *)ft_calloc(sizeof(char), end - start + 1);
+	if (!word)
 		return (NULL);
 	i = 0;
 	while (i + start < end)
 	{
-		token[i] = line[i + start];
+		word[i] = line[i + start];
 		i++;
 	}
-	printf("token %s\n", token);
+	return (word);
+}
+
+t_token	*create_token_node(char *word, int expansion_flg)
+{
+	t_token	*token;
+
+	token = (t_token *)ft_calloc(sizeof(t_token), 1);
+	if (!token)
+		return (NULL); // TODO error
+	token->word = word;
+	token->expansion_flg = expansion_flg;
+	token->next = NULL;
 	return (token);
 }
 
-t_tokens	*create_token_node(char *word)
+void	add_token(t_token **head, t_token *new)
 {
-	t_tokens	*token;
+	t_token	*temp;
 
-	token = (t_tokens *)ft_calloc(sizeof(t_tokens), 1);
-	if (!token)
-		return (NULL); // TODO error
-	token->token = word;
-	token->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+}
+
+void	syntax_error(void)
+{
+	perror("syntax_error");
+	// free token
+	// free cmd
 }
