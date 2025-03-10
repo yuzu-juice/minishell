4832e88b55978f467e7bb980ad285996cce99adc
Yoshiko Hatanaka <yohatana@c3r5s1.42tokyo.jp>
2025-03-10 19:43:17 +0900

    create space split token

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 18:22:31 by yohatana          #+#    #+#             */
-/*   Updated: 2025/03/09 17:54:43 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/03/10 19:42:32 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,16 @@ typedef struct s_token t_token;
 typedef struct s_token
 {
 	char	*word;
-	int		expansion_flg;
 	t_token	*next;
 }	t_token;
 
-t_token		*create_token_node(char *word, int expansion_flg);
+t_token		*create_token_node(char *word);
 void		add_token(t_token **head, t_token *new);
-static char	*quart_token(int start, int end, char *line);
+static char	*create_token_word(int start, int end, char *line);
 void		syntax_error(void);
 
+// 空白区切りのみ許容します！！！！！！！
+// ゴミを読み込んでるので初期化すること
 void	parser(int argc, char **envp, char *line)
 {
 	(void)argc;
@@ -50,93 +51,62 @@ void	parser(int argc, char **envp, char *line)
 	int		j;
 	char	*word;
 	t_token	*token_head;
-	char	*quart_del_line;
 
 	token_head = NULL;
 	i = 0;
 	j = 0;
-	int	expansion_flg = 0;
 	while (i < (int)ft_strlen(line))
 	{
-		// echo"aaa"のときは別のtokenにしちゃうとまずい（ゆたtips）
-		// のでいっかいクォートを削除するほうが丸いと思った
-		// tokenのwordに入れはするが、これ単体では入れない
-
-		// $展開をすべて後回しにすると、シングルでも展開されてしまう
-		// token→wordに入れた時点では、完全な文字列にしなくてはいけない
-
-		// bash manual tokanizerを参照
-		// 'が奇数のときはエラーにする
-		if (line[i] == '\'')
+		if (line[i] != ' ')
 		{
-			j = i + 1;
-			while (line[j] != '\'')
-				j++;
-			if (line[j] == '\'' && j < (int)ft_strlen(line))
-			{
-				word = quart_token(i + 1, j , line);
-				add_token(&token_head, create_token_node(word, expansion_flg));
-				i = j;
-			}
-			if (j == (int)ft_strlen(line))
-			{
-				syntax_error();
-			}
-			// 次の文字が空白かパイプ以外ならwordにstrjoinする
-			if (line[i + 1] != ' ' || line[i + 1] != '|' )
+			if (line[i] == '\'')
 			{
 				j = i + 1;
 				while (line[j] != '\'')
 					j++;
 				if (line[j] == '\'' && j < (int)ft_strlen(line))
 				{
-					word = quart_token(i + 1, j , line);
-					// last node->word = ft_strjoin(node->word, word);
-					// add_token(&token_head, create_token_node(word, expansion_flg));
+					word = create_token_word(i, j , line);
+					add_token(&token_head, create_token_node(word));
 					i = j;
 				}
+				if (j == (int)ft_strlen(line))
+					syntax_error();
+			}
+			else if (line[i] == '\"')
+			{
+				j = i + 1;
+				while (line[j] != '\"')
+					j++;
+				if (line[j] == '\"')
+				{
+					word = create_token_word(i, j , line);
+					add_token(&token_head, create_token_node(word));
+					i = j;
+				}
+				if (j == (int)ft_strlen(line))
+					syntax_error();
+			}
+			else
+			{
+				j = i;
+				while ((line[j] != ' ' && line[j] != '|') && line[j] != '\0' )
+					j++;
+				word = create_token_word(i, j, line);
+				add_token(&token_head, create_token_node(word));
+				i = j;
 			}
 		}
-		// else if (line[i] == '\"')
-		// {
-		// 	j = i + 1;
-		// 	while (line[j] != '\"')
-		// 		j++;
-		// 	if (line[j] == '\"')
-		// 	{
-		// 		word = quart_token(i + 1, j , line);
-		// 		expansion_flg = 1;
-		// 		add_token(&token_head, create_token_node(word, expansion_flg));
-		// 		i = j;
-		// 	}
-		// 	if (j == (int)ft_strlen(line))
-		// 	{
-		// 		syntax_error();
-		// 	}
-		// }
+
+		// こっちはあと
+		/*
 		// pipeを先にやる
 		else if (line[i] == '|')
 		{
-			// create token
-			// word = "|"
+			word = create_token_word(i, i , line);
+			add_token(&token_head, create_token_node(word));
 		}
-		else if (line[i] == '$')
-		{
-			// ' ' '\'' "\""が来るまでtokenとして扱う
-		}
-		else
-		{
-			j = i;
-			while ((line[j] != ' ' && line[j] != '\'' \
-			&& line[j] != '\"' && line[j] != '$'\
-			&& line[j] != '|') && line[j] != '\0' )
-			{
-				j++;
-			}
-			word = quart_token(i, j , line);
-			add_token(&token_head, create_token_node(word, expansion_flg));
-			i = j - 1;
-		}
+		*/
 		i++;
 	}
 
@@ -146,16 +116,12 @@ void	parser(int argc, char **envp, char *line)
 	test = token_head;
 	while (test)
 	{
-		printf("word :%s expansion flg :%d\n", test->word, test->expansion_flg);
+		printf("word :%s\n", test->word);
 		test = test->next;
 	}
-
-	// '|'が来るまでは連結する
-	// '|'の前後にtokenがない場合(> ,'', "", $, stringはtokenになる)
-
 }
 
-static char	*quart_token(int start, int end, char *line)
+static char	*create_token_word(int start, int end, char *line)
 {
 	char	*word;
 	int		i;
@@ -165,7 +131,7 @@ static char	*quart_token(int start, int end, char *line)
 	if (!word)
 		return (NULL);
 	i = 0;
-	while (i + start < end)
+	while (i + start <= end)
 	{
 		word[i] = line[i + start];
 		i++;
@@ -173,7 +139,7 @@ static char	*quart_token(int start, int end, char *line)
 	return (word);
 }
 
-t_token	*create_token_node(char *word, int expansion_flg)
+t_token	*create_token_node(char *word)
 {
 	t_token	*token;
 
@@ -181,7 +147,6 @@ t_token	*create_token_node(char *word, int expansion_flg)
 	if (!token)
 		return (NULL); // TODO error
 	token->word = word;
-	token->expansion_flg = expansion_flg;
 	token->next = NULL;
 	return (token);
 }
@@ -237,6 +202,13 @@ int	main(void)
 	parser(0, NULL, "echo \"$PWD\""); // work//minishell
 	parser(0, NULL, "\"echo $PWD\""); // work//minishell
 	parser(0, NULL, "echo \"hello 'world'\""); // hello 'world'
+
+	// これがどうなる？
+	parser(0, NULL, echo "hello'world$PWD'$PWD"); //
+
+	// yohatana@c3r5s1:~/work/minishell$ echo hello'world''$PWD'"$PWD"
+	// helloworld$PWD/home/yohatana/work/minishell
+
 	return (0);
 }
 */
