c349af508f1b0698ddd84eb27d4e30b87bc0b669
yuzu-juice <yuzujuicesec@gmail.com>
2025-03-16 12:44:05 +0900

    implemented some tests

--- tests/integration/exec.test.sh
+++ tests/integration/exec.test.sh
@@ -1,8 +1,9 @@
 . $(dirname $0)/utils/assert.sh
 
 assert 'ls -t'
-assert '/usr/bin/ls -t'
+assert '/usr/bin/ls'
 assert 'cat Makefile'
+assert '/usr/bin/cat Makefile'
 assert 'echo'
 assert 'echo aaa'
 assert 'echo "aaa"sss "ddd"'
