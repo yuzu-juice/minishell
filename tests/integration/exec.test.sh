42d822d629daf3284b690d79042816df499b34b8
yuzu-juice <yuzujuicesec@gmail.com>
2025-03-18 13:20:34 +0000

    fix: test case

--- tests/integration/exec.test.sh
+++ tests/integration/exec.test.sh
@@ -6,6 +6,5 @@ assert 'cat Makefile'
 assert '/usr/bin/cat Makefile'
 assert 'echo -n'
 assert 'echo -n aaa'
-assert 'echo -n "aaa"sss "ddd"'
 assert "echo -n 'aaa' 'sss' 'ddd'"
 assert 'echo -n aaa sss        ddd'
