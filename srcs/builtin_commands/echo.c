0e3f69d22def747a02fc072fc722b971dc4c6abb
yuzu-juice <yuzujuicesec@gmail.com>
2025-03-18 12:46:57 +0000

    update: echo behaves with option n

--- srcs/builtin_commands/echo.c
+++ srcs/builtin_commands/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: takitaga <takitaga@student.42.fr>          +#+  +:+       +#+        */
+/*   By: takitaga <takitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 10:45:06 by takitaga          #+#    #+#             */
-/*   Updated: 2025/03/16 12:27:58 by takitaga         ###   ########.fr       */
+/*   Updated: 2025/03/18 21:35:10 by takitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ void	echo(int argc, char **argv)
 
 	i = 1;
 	if (argc == 1)
-		write(1, "\n", 1);
+		return ;
 	while (i < argc)
 	{
 		write(1, argv[i], ft_strlen(argv[i]));
 		write(1, " ", 1);
 		i++;
 	}
-	write(1, "\n", 1);
 }
 
 // int main(int argc, char **argv)
