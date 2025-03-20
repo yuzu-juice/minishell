f6bd97a2ccd47d41fe59a988e9bdc15d947f409f
yuzu-juice <yuzujuicesec@gmail.com>
2025-03-20 14:58:14 +0000

    fix: norm

--- srcs/builtin_commands/pwd.c
+++ srcs/builtin_commands/pwd.c
@@ -6,29 +6,31 @@
 /*   By: takitaga <takitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 19:03:10 by takitaga          #+#    #+#             */
-/*   Updated: 2025/03/20 14:56:18 by takitaga         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:57:58 by takitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include <stdio.h>
 
-static void	error(void)
-{
-	write(2, "Usage: pwd\n", 11);
-}
+static void	error(void);
 
 void	pwd(int argc)
 {
-  char *pwd;
+	char	*pwd;
 
-  if (argc != 1)
-  {
-    error();
-    return ;
-  }
-  pwd = getcwd(NULL, 0);
-  write(1, pwd, ft_strlen(pwd));
-  write(1, "\n", 1);
-  free(pwd);
+	if (argc != 1)
+	{
+		error();
+		return ;
+	}
+	pwd = getcwd(NULL, 0);
+	write(1, pwd, ft_strlen(pwd));
+	write(1, "\n", 1);
+	free(pwd);
+}
+
+static void	error(void)
+{
+	write(2, "Usage: pwd\n", 11);
 }
