244a33b06b8c7922052f19b05af813cbb2a46f94
yuzu-juice <yuzujuicesec@gmail.com>
2025-03-27 21:11:28 +0900

    feat: input redirection

---
+++ tests/unit/redirect_input.test.c
@@ -0,0 +1,55 @@
+#include "../../includes/minishell.h"
+
+int main() {
+    t_proc *process;
+    t_redirection *redir1;
+    t_env *env;
+
+    process = ft_calloc(1, sizeof(t_proc));
+    process->cmd = "cat";
+    redir1 = ft_calloc(1, sizeof(t_redirection));
+    redir1->type = INPUT;
+    redir1->filename = "input.txt";
+    env = ft_calloc(1, sizeof(t_env));
+    env->key = ft_strdup("HOME");
+    env->value = ft_strdup("/home/user");
+
+    // input.txt に "hoge" を書き込む
+    int fd = open(redir1->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    write(fd, "hoge", 4);
+    close(fd);
+
+    int pipe_fd[2];
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe failed");
+        return 1;
+    }
+
+    int pid = fork();
+    if (pid == 0) {
+        close(pipe_fd[0]);
+        dup2(pipe_fd[1], STDOUT_FILENO);
+        close(pipe_fd[1]);
+
+        redirect(process->cmd, redir1, env);
+    } else {
+        close(pipe_fd[1]);
+
+        int status;
+        waitpid(pid, &status, 0);
+
+        char buffer[100];
+        int bytes_read = read(pipe_fd[0], buffer, sizeof(buffer) - 1);
+        assert(strcmp(buffer, "hoge") == 0);
+        close(pipe_fd[0]);
+    }
+
+    unlink("input.txt");
+    free(env->key);
+    free(env->value);
+    free(env);
+    free(redir1);
+    free(process);
+
+    return (0);
+}
