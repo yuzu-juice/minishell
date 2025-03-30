fea50886ff87c5e52bfd66b008a41c44b15c162d
Yoshiko Hatanaka <yohatana@c5r3s9.42tokyo.jp>
2025-03-30 13:26:34 +0900

    delete many comment

--- srcs/expand_dollar.c
+++ srcs/expand_dollar.c
@@ -6,7 +6,7 @@
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:33:45 by yohatana          #+#    #+#             */
-/*   Updated: 2025/03/30 13:21:04 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/03/30 13:25:47 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,16 +107,6 @@ static bool	replace_env_word(t_token *curr, int *index, t_env *env)
 	return (false);
 }
 
-// char	*serch_env_value(char *key, t_env *env)
-// {
-// 	t_env	*find;
-
-// 	find = search_env(key, env);
-// 	if (find == NULL)
-// 		return ("");
-// 	return (find->value);
-// }
-
 static char	*split_env_key(t_token *curr, int *index)
 {
 	char	*key;
@@ -131,39 +121,3 @@ static char	*split_env_key(t_token *curr, int *index)
 		return (NULL);
 	return (key);
 }
-
-// bool	replace_word(t_token *curr, int *index, t_env replace)
-// {
-// 	char	*after_token_word;
-// 	char	*temp;
-// 	char	*dollar_after_word;
-
-// 	after_token_word = ft_calloc(ft_strlen(curr->word) - \
-// 						ft_strlen(replace.key) + ft_strlen(replace.value), \
-// 						sizeof(char));
-// 	if (!after_token_word)
-// 		return (true);
-// 	after_token_word = ft_substr(curr->word, 0, *index);
-// 	temp = ft_strjoin(after_token_word, replace.value);
-// 	free(after_token_word);
-// 	dollar_after_word = split_after_word(curr, index, replace);
-// 	if (!dollar_after_word)
-// 		return (true);
-// 	after_token_word = ft_strjoin(temp, dollar_after_word);
-// 	free(curr->word);
-// 	free(temp);
-// 	if (!after_token_word)
-// 		return (true);
-// 	curr->word = after_token_word;
-// 	return (false);
-// }
-
-// char	*split_after_word(t_token *curr, int *index, t_env replace)
-// {
-// 	char	*split_after_word;
-
-// 	split_after_word = ft_substr(curr->word, \
-// 								*index + ft_strlen(replace.key), \
-// 								ft_strlen(curr->word));
-// 	return (split_after_word);
-// }
