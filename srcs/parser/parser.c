bc6745686fc95dc907b487c8a29309f4e867b583
Yoshiko Hatanaka <yohatana@c5r3s9.42tokyo.jp>
2025-03-30 18:28:05 +0900

    make parser dir

---
+++ srcs/parser/parser.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 18:22:31 by yohatana          #+#    #+#             */
+/*   Updated: 2025/03/30 18:24:12 by yohatana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+bool	parser(char *line)
+{
+	t_token	*head;
+	t_proc	*proc;
+
+	head = NULL;
+	if (has_unclosed_quotes(line))
+	{
+		syntax_error(&head);
+		return (true);
+	}
+	head = create_token_list(line);
+	if (!head)
+		return (true);
+	proc = create_process_list(&head);
+	if (!proc)
+	{
+		ft_putendl_fd("syntax_error", 2);
+		return (true);
+	}
+	return (false);
+}
+
+bool	has_unclosed_quotes(char *line)
+{
+	int		i;
+	int		j;
+	char	type;
+
+	i = 0;
+	j = 0;
+	type = 0;
+	while (line[i])
+	{
+		if (line[i] == '\'' || line[i] == '\"')
+		{
+			if (line[i] == '\'')
+				type = '\'';
+			if (line[i] == '\"')
+				type = '\"';
+			j = i + 1;
+			while (line[j] != type && j < (int)ft_strlen(line))
+				j++;
+			if (j == (int)ft_strlen(line))
+				return (true);
+			i = j;
+		}
+		i++;
+	}
+	return (false);
+}
+
+void	syntax_error(t_token **head)
+{
+	ft_putendl_fd("syntax_error", 2);
+	free_token_list(head);
+}
