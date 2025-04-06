fcb8019e00097ef0d8482577e20e823472729abe
Yoshiko Hatanaka <yohatana@c6r2s3.42tokyo.jp>
2025-04-06 12:18:45 +0900

    forget commit

--- srcs/builtin_commands/export.c
+++ srcs/builtin_commands/export.c
@@ -6,7 +6,7 @@
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 18:42:27 by yohatana          #+#    #+#             */
-/*   Updated: 2025/04/01 14:19:00 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/04/06 12:05:53 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,11 @@ void	export(int argc, char **argv, t_env **env)
 	}
 	else if (argc == 2)
 	{
-		printf("argv[0] %s\n", argv[0]);
-		printf("argv[1] %s\n", argv[1]);
-		printf("argv[2] %s\n", argv[2]);
 		add_env_node(*env, argv[1], 1);
-		print_env(env);
 	}
 	else
 	{
-		ft_putendl_fd("too many args", 2);
+		ft_putstr_fd("Usage: export [key=value]\n", 2);
 	}
 }
 
@@ -41,7 +37,6 @@ static void	print_env(t_env **env)
 	tmp = *env;
 	while (tmp)
 	{
-		printf("print_env() : tmp %p\n", tmp);
 		if (tmp->value == NULL)
 		{
 			write(1, tmp->key, ft_strlen(tmp->key));
