8bb27f2f7822d830da1b70b3a8b492d4c622228d
yohanata42 <yoshikohatanaka1998@gmail.com>
2025-04-16 17:50:53 +0900

    init minishell struct

--- tests/unit/expand_dollar.test.c
+++ tests/unit/expand_dollar.test.c
@@ -10,8 +10,8 @@ int main(int argc, char **argv, char **envp)
 	int i = 0;
 	t_minishell *m_shell;
 
-	env = envp_to_list(envp);
 	m_shell->env = env;
+	m_shell = create_minishell_struct(env);
 
 	// only 1 token 1 dollar
 	char	*array1[] = {getenv("PWD")};
