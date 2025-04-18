a447ea4506f7d13f97cc28c573eeca30e410d0ed
Yoshiko Hatanaka <yohatana@c6r3s6.42tokyo.jp>
2025-04-18 13:48:43 +0900

    quote comp

--- srcs/utils/remove_quotes.c
+++ srcs/utils/remove_quotes.c
@@ -3,58 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   remove_quotes.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: takitaga <takitaga@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 11:33:20 by takitaga          #+#    #+#             */
-/*   Updated: 2025/03/16 12:28:57 by takitaga         ###   ########.fr       */
+/*   Updated: 2025/04/18 13:48:08 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 static int	count_quotes(char *token);
+static char	*copy_token(char *token, char *ret);
 
 char	*remove_quotes(char *token)
 {
 	char	*ret;
 	int		remove_char_count;
-	int		i;
 
 	remove_char_count = count_quotes(token);
-	i = 0;
 	if (remove_char_count == 0)
 		return (token);
 	ret = ft_calloc(ft_strlen(token) - remove_char_count, sizeof(char));
 	if (ret == NULL)
 		return (NULL);
-	i = 0;
-	while (*token != '\0')
-	{
-		if (*token == '\'' || *token == '"')
-		{
-			token++;
-			continue ;
-		}
-		ret[i] = *token;
-		token++;
-		i++;
-	}
-	ret[i] = '\0';
+	ret = copy_token(token, ret);
 	return (ret);
 }
 
 static int	count_quotes(char *token)
 {
-	int	remove_char_count;
-	int	i;
+	int		remove_char_count;
+	int		i;
+	int		j;
+	char	quote_type;
 
 	remove_char_count = 0;
 	i = 0;
+	quote_type = 0;
 	while (token[i] != '\0')
 	{
 		if (token[i] == '\'' || token[i] == '"')
+		{
 			remove_char_count++;
+			quote_type = token[i];
+			j = i + 1;
+			while (token[j] != quote_type)
+				j++;
+			remove_char_count++;
+			i = j;
+		}
 		i++;
 	}
 	return (remove_char_count);
+}
+
+static char	*copy_token(char *token, char *ret)
+{
+	int		i;
+	char	quote_type;
+	int		j;
+	int		k;
+
+	i = 0;
+	k = 0;
+	while (token[i] != '\0')
+	{
+		if (token[i] == '\'' || token[i] == '"')
+			quote_type = token[i];
+		j = i + 1;
+		while (token[j] != quote_type)
+		{
+			ret[k] = token[j];
+			j++;
+			k++;
+		}
+		i = j;
+		i++;
+	}
+	ret[k] = '\0';
+	return (ret);
 }
