6ad174ec1b6351ef82809997af30da108fe1a230
Yoshiko Hatanaka <yohatana@c6r3s7.42tokyo.jp>
2025-04-19 21:09:11 +0900

    norming

--- srcs/builtin_commands/cd.c
+++ srcs/builtin_commands/cd.c
@@ -6,30 +6,15 @@
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 11:15:38 by takitaga          #+#    #+#             */
-/*   Updated: 2025/04/19 21:01:43 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/04/19 21:06:30 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 static void	error(void);
-// static char	*get_relative_path(char *relative_path, char *argv);
+static int	return_error(char *argv);
 
-// TODO
-/*
-	親でも子でも変わらない
-	yohatana@c6r3s7:~/work/minishell$ chmod 000 aa
-	yohatana@c6r3s7:~/work/minishell$ cd aa
-	bash: cd: aa: Permission denied
-	yohatana@c6r3s7:~/work/minishell$ echo $?
-	1
-	yohatana@c6r3s7:~/work/minishell$ cd no
-	bash: cd: no: No such file or directory
-	yohatana@c6r3s7:~/work/minishell$ echo $?
-	1
-
-	OLDPWD 前のディレクトリを保管できる（めっちゃ使う）
-*/
 int	cd(int argc, char **argv)
 {
 	char	*relative_path;
@@ -43,28 +28,25 @@ int	cd(int argc, char **argv)
 	if (ft_strncmp(argv[1], "..", 2) != 0)
 	{
 		if (access(argv[1], X_OK) != 0)
-		{
-			perror(argv[1]);
-			return (1);
-		}
+			return (return_error(argv[1]));
 	}
 	if (access(argv[1], F_OK) == 0)
 	{
 		if (chdir(argv[1]) == -1)
-		{
-			perror(argv[1]);
-			return (1);
-		}
+			return (return_error(argv[1]));
 	}
 	else
-	{
-		perror(argv[1]);
-		return (1);
-	}
+		return (return_error(argv[1]));
 	return (0);
 }
 
 static void	error(void)
 {
 	ft_putendl_fd("Usage: cd [relative path | absolute path]", 2);
+}
+
+static int	return_error(char *argv)
+{
+	perror(argv);
+	return (1);
 }
