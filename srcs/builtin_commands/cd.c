211b012ebf1628628acfb5435a3281cea0bcca72
Yoshiko Hatanaka <yohatana@c6r3s7.42tokyo.jp>
2025-04-19 20:41:26 +0900

    delete access(argv[1], X_OK)

--- srcs/builtin_commands/cd.c
+++ srcs/builtin_commands/cd.c
@@ -6,13 +6,14 @@
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 11:15:38 by takitaga          #+#    #+#             */
-/*   Updated: 2025/04/19 18:55:12 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/04/19 20:40:38 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 static void	error(void);
+// static char	*get_relative_path(char *relative_path, char *argv);
 
 // TODO
 /*
@@ -26,15 +27,22 @@ static void	error(void);
 	bash: cd: no: No such file or directory
 	yohatana@c6r3s7:~/work/minishell$ echo $?
 	1
+
+	cd .. これ対応する
+	OLDPWD 前のディレクトリを保管できる（めっちゃ使う）
 */
 int	cd(int argc, char **argv)
 {
+	char	*relative_path;
+
+	relative_path = NULL;
 	if (argc != 2)
 	{
 		error();
 		return (1);
 	}
-	if (access(argv[1], F_OK) == 0 && access(argv[1], X_OK))
+	// ..はじまりのときaccess(argv[1], X_OK)が失敗する可能性
+	if (access(argv[1], F_OK) == 0)
 	{
 		if (chdir(argv[1]) == -1)
 		{
