f720fcb7dabf34627fe9203938b38673bdf2da76
Yoshiko Hatanaka <yohatana@c6r3s4.42tokyo.jp>
2025-04-23 14:17:47 +0900

    can ls, l's', lsetc...

--- srcs/exec_cmd.c
+++ srcs/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 14:11:36 by yohatana          #+#    #+#             */
-/*   Updated: 2025/04/20 19:32:33 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/04/23 14:16:38 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	exec_cmd(t_minishell *m_shell, \
 	{
 		exit(exec_builtin(proc->cmd_args, builtin_cmd, m_shell));
 	}
-	cmd_path = create_cmd_path(proc->cmd, m_shell);
+	cmd_path = create_cmd_path(proc->cmd_args[0], m_shell);
 	if (cmd_path == NULL)
 	{
 		perror(NULL);
