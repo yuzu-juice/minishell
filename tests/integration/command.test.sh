cb0622e2340e637deaadc47040a358eff09210e7
Yoshiko Hatanaka <yohatana@c6r3s4.42tokyo.jp>
2025-04-23 15:01:20 +0900

    add test case , but dont move

--- tests/integration/command.test.sh
+++ tests/integration/command.test.sh
@@ -10,8 +10,16 @@ assert_bash_equivalent "ls | cat | wc -l"
 
 # リダイレクトのテスト
 assert_bash_equivalent ls > comand.txt
+# assert_bash_equivalent "<< eof"
+# assert_bash_equivalent "> outfile"
+# assert_bash_equivalent "< infile"
+# assert_bash_equivalent ">> outfile_add"
+
+# rm outfile
+# rm infile
+# rm outfile_add
+rm comand.txt
 
 # 2000cat
 assert_bash_equivalent ./tests/integration/utils/2000cat.sh
 
-rm comand.txt
