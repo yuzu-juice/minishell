5ca9d8be104ad9a1baba388737d15d03e3d44813
yohanata42 <yoshikohatanaka1998@gmail.com>
2025-04-24 22:17:57 +0900

    delete unuse line

--- tests/integration/command.test.sh
+++ tests/integration/command.test.sh
@@ -9,7 +9,7 @@ assert_bash_equivalent "ls | cat"
 assert_bash_equivalent "ls | cat | wc -l"
 
 # リダイレクトのテスト
-# assert_bash_equivalent ls > comand.txt
+assert_bash_equivalent ls > comand.txt
 # assert_bash_equivalent "<< eof"
 # assert_bash_equivalent "> outfile"
 # assert_bash_equivalent "< infile"
@@ -18,7 +18,7 @@ assert_bash_equivalent "ls | cat | wc -l"
 # rm outfile
 # rm infile
 # rm outfile_add
-# rm comand.txt
+rm comand.txt
 
 # 2000cat
 assert_bash_equivalent ./tests/integration/utils/2000cat.sh
@@ -36,4 +36,3 @@ assert_bash_equivalent "echo '$USER'"
 # assert_bash_equivalent 'echo \'"$USER"\''
 # assert_bash_equivalent 'echo "\'$USER"\''
 
-rm comand.txt
